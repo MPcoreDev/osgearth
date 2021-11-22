be39faaece4b1281c222bc66f4c75e0a813672af
Jérôme DESFIEUX <jerome.desfieux@navblue.aero>
2021-11-22 18:39:24 +0100

    D-27220 Correct the backface culling

--- src/osgEarth/MPLineDrawable.glsl
+++ src/osgEarth/MPLineDrawable.glsl
@@ -6,6 +6,7 @@ $GLSL_DEFAULT_PRECISION_FLOAT
 #pragma vp_location vertex_model
 #pragma vp_order last
 
+uniform mat4 osg_ViewMatrix;
 
 // Ratio between the local sclae and the pixel scale
 uniform float mp_local2pixel;
@@ -39,7 +40,7 @@ void mp_MPLineDrawable_VS_MODEL(inout vec4 vertex)
     float widthByTwo = mp_LineWidth*0.5;
 
     // check if this vertex is at the opposite side of the globe
-    vec4 earthCenter_view = gl_ModelViewMatrix * vec4(0., 0., 0., 1.);
+    vec4 earthCenter_view = osg_ViewMatrix * vec4(0., 0., 0., 1.);
     vec3 normal = (gl_ModelViewMatrix*vertex).xyz - earthCenter_view.xyz;
     mp_MPLineDrawable_backFaceCulled = normal.z < 0. ? 1 : 0;
 
