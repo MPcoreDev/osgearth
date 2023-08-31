725e5bf148e2ae859500a196b50f78b3a0365993
Jérôme DESFIEUX <jerome.desfieux@navblue.aero>
2023-08-31 14:47:46 +0200

    S-73883 quality

--- src/osgEarthAnnotation/MPAnnotationGroupSG.cpp
+++ src/osgEarthAnnotation/MPAnnotationGroupSG.cpp
@@ -17,7 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-#include "osgEarth/Notify"
 #include <algorithm>
 #include <osgEarthAnnotation/MPAnnotationGroupSG>
 #include <osgEarthAnnotation/MPAnnotationDrawable>
